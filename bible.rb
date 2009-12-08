bc4b3a1a1e189dfe80ec142083691837256848e9
Gadi Aleksandrowicz <gadial@gadial-desktop.(none)>
2009-12-08 23:00:43 +0200

    Working on distance between ELS

--- bible.rb
+++ bible.rb
@@ -26,11 +26,25 @@ class Els
   def include?(i)
     indices.include?(i)
   end
+  def start_point
+    n
+  end
+  def end_point
+    n+(k-1)*d
+  end
   def to_s
     "ELS of #{self.word}: n=#{self.n}, d=#{self.d}, k=#{self.k}"
   end
   def inspect
     to_s
+  end
+  def distance(rhs,d)
+    indices.collect do |i|
+      rhs.indices.collect{|j| ((i%d)-(j%d)).abs + ((i/d)-(j/d)).abs}.min
+    end.min
+  end
+  def min_distance(rhs)
+    (0..20).collect{|d| [distance(rhs,d),d]}.sort.min
   end
 end
 
@@ -57,6 +71,19 @@ class Text
     end
     results.collect{|d, arr| arr.collect{|n| Els.new(word,n,d,word.length,@@default_els_color)}}.flatten
   end
+  def find_min(word, max_jump)
+    result = []
+    for d in 1..max_jump do
+      regexp = Regexp.new(word.split("").collect{|s| s+"."*(d-1)}.join("")[0..-d])
+      temp = @text.index(regexp)
+      while temp != nil
+	  result << temp
+	  temp = @text.index(regexp, temp+1)
+      end
+      return result.collect{|n| Els.new(word,n,d,word.length,@@default_els_color)} unless result.empty?
+    end    
+    return []
+  end
   def show(from, to, jump, els_array)
     base_color = 41
     column = 0
@@ -67,27 +94,53 @@ class Text
       print colorize(@text[i..i],color)
       column += 1
       if column == jump
-      column = 0
-      puts
+	column = 0
+	puts
       end
     end
+    puts unless column == 0
   end
-#   def show(n,d,k,color)
-#     gap = 30
-#     a = [0, n-gap].max
-#     b = [@text.length-1, n+d*(k-1)].min
-#       subtext = @text[a..b]
-#       pos = a
-#       col = 0
-#       while pos < b
-# 	
-#       end
-#   end
 end
 
-t = Text.from_file("alice.txt")
-# els = Els.new(100,5,20,42)
-# t.show(80,200,5,[els])
-# els.
-puts t.find("rabin",200).inspect
+class Organizer
+  @@max_jump = 200
+  attr_accessor :text, :words
+  def Organizer.from_file(filename)
+    Organizer.new(Text.from_file(filename))
+  end
+  def initialize(text)
+    self.text = text
+    self.words = {}
+  end
+  def read_words_from_file(filename)
+    File.open(filename, "r"){|file| file.read.split("\n")}.each do |word|
+      temp = self.text.find_min(word, @@max_jump)
+      self.words[word] = temp unless temp.empty?
+    end
+  end
+  def words_found
+    self.words.collect{|key, item| key}
+  end
+  def inspect
+    self.words.inspect
+  end
+end
+
+o = Organizer.from_file("alice.txt")
+o.read_words_from_file("words.txt")
+# puts 
+puts o.words_found.inspect
+
+
+
+
+# t = Text.from_file("alice.txt")
+# # els = Els.new(100,5,20,42)
+# # t.show(80,200,5,[els])
+# # els.
+# els_array = t.find_min("gadi",300)
+# puts els_array.inspect
+# temp = els_array.first
+# t.show(temp.start_point - 50, temp.end_point + 50, temp.d+10, els_array) unless temp == nil
+# 
 # puts "Good morning #{colorize("world",42)}!"
